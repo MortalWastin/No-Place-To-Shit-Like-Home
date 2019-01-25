85f5dcb3fddb70c3cf4475ce069e477a764b6aab
Unknown <marciosr@live.com.pt>
2019-01-25 22:09:44 +0000

    Initial Project Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	Vector3 moveInput = Vector3.zero;
+	float speed = 7f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float h = Input.GetAxis("Horizontal");
+		float v = Input.GetAxis("Vertical");
+
+		moveInput = new Vector3(h, 0.0f, v) * speed;
+
+		transform.position += moveInput * Time.deltaTime;
+
+	}
+}
