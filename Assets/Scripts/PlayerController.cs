7472d47487194e13947820046fc84a1a45b3e236
Diogo Portela <diogo.ribeiro.portela@gmail.com>
2019-01-26 12:05:12 +0000

    Interaction and GameManager

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,89 +1,100 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-	public float speed;
-	[Range(0, 1)]
-	public float rotationSpeed;
-	public Transform pickUp_UI;
-	public List<Transform> pickUp_Objs;
-	public Transform pickable;
-	public bool inRange = false;
+    public float speed;
+    [Range(0, 1)]
+    public float rotationSpeed;
+    public Transform pickUp_UI;
+    private List<PickUpObject> pickUp_Obj_List;
+    private PickUpObject closestPickUpObject;
 
-	private void Start()
-	{
-		pickUp_Objs = new List<Transform>();
+    public bool isMovable;
 
-	}
+    private void Start()
+    {
+        pickUp_Obj_List = new List<PickUpObject>();
+        isMovable = true;
+    }
 
-	void Update()
-	{
-		Movement();
+    void Update()
+    {
+        if (isMovable)
+        {
+            Movement();
+            Interact();
+        }
+    }
 
-		if(pickUp_Objs != null)
-			ObjectsDistance();
+    private void Interact()
+    {
+        if (pickUp_Obj_List.Count > 0)
+        {
+            ObjectsDistance();
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                GameManager.Instance.AddItem(closestPickUpObject);
+                OnTriggerExit(closestPickUpObject.GetComponent<Collider>());
+            }
+        }
+    }
+    private void Movement()
+    {
+        Vector3 deltaPosition = Vector3.zero;
+        if (Input.GetKey(PlayerSettings.Instance.Up))
+        {
+            deltaPosition += Vector3.forward * speed * Time.deltaTime;
+        }
+        else if (Input.GetKey(PlayerSettings.Instance.Down))
+        {
+            deltaPosition += Vector3.back * speed * Time.deltaTime;
+        }
 
-		if(inRange && Input.GetKeyDown(KeyCode.F))
-		{
-			Destroy(pickable.gameObject);
-		}
-	}
+        if (Input.GetKey(PlayerSettings.Instance.Left))
+        {
+            deltaPosition += Vector3.left * speed * Time.deltaTime;
+        }
+        else if (Input.GetKey(PlayerSettings.Instance.Right))
+        {
+            deltaPosition += Vector3.right * speed * Time.deltaTime;
+        }
+        transform.LookAt(Vector3.Lerp(transform.position + transform.forward, transform.position + deltaPosition, rotationSpeed));
+        transform.position += deltaPosition;
+    }
+    private void ObjectsDistance()
+    {
+        float distance = 0.0f;
+        foreach (PickUpObject t in pickUp_Obj_List)
+        {
+            float d = Vector3.Distance(this.transform.position, t.transform.position);
+            if (distance > d || distance == 0)
+            {
+                distance = d;
+                closestPickUpObject = t;
+            }
+        }
+    }
 
-	private void Movement()
-	{
-		Vector3 deltaPosition = Vector3.zero;
-		if (Input.GetKey(PlayerSettings.Instance.Up))
-		{
-			deltaPosition += Vector3.forward * speed * Time.deltaTime;
-		}
-		else if (Input.GetKey(PlayerSettings.Instance.Down))
-		{
-			deltaPosition += Vector3.back * speed * Time.deltaTime;
-		}
-
-		if (Input.GetKey(PlayerSettings.Instance.Left))
-		{
-			deltaPosition += Vector3.left * speed * Time.deltaTime;
-		}
-		else if (Input.GetKey(PlayerSettings.Instance.Right))
-		{
-			deltaPosition += Vector3.right * speed * Time.deltaTime;
-		}
-		transform.LookAt(Vector3.Lerp(transform.position + transform.forward, transform.position + deltaPosition, rotationSpeed));
-		transform.position += deltaPosition;
-	}
-
-	private void ObjectsDistance()
-	{
-		float distance = 0.0f;
-		foreach (Transform t in pickUp_Objs)
-		{
-			float d = Vector3.Distance(this.transform.position, t.position);
-			if (distance > d || distance == 0)
-			{
-				distance = d;
-				pickable = t;
-			}
-		}
-	}
-
-	private void OnTriggerEnter(Collider other)
-	{
-		if (other.gameObject.layer == LayerMask.NameToLayer("PickUp"))
-		{
-			pickUp_UI.gameObject.SetActive(true);
-			pickUp_Objs.Add(other.transform);
-			inRange = true;
-		}
-	}
-
-	private void OnTriggerExit(Collider other)
-	{
-		pickUp_UI.gameObject.SetActive(false);
-		pickUp_Objs.Remove(other.transform);
-		inRange = false;
-	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("PickUp"))
+        {
+            PickUpObject puo = other.GetComponent<PickUpObject>();
+            pickUp_UI.gameObject.SetActive(true);
+            pickUp_Obj_List.Add(puo);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("PickUp"))
+        {
+            PickUpObject puo = other.GetComponent<PickUpObject>();
+            pickUp_Obj_List.Remove(puo);
+            if (pickUp_Obj_List.Count == 0)
+                pickUp_UI.gameObject.SetActive(false);
+            //else
+                //Atualizar UI
+        }
+    }
 }
