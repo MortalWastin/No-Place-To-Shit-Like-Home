871e9f5eb04b7d5cba8b9d8e9598da1704a12faa
Diogo Portela <diogo.ribeiro.portela@gmail.com>
2019-01-27 14:43:55 +0000

    lotta stuff

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,7 +10,7 @@ public class GameManager : MonoBehaviour
     public GameObject poopPrefab;
     private PlayerController currentPlayer;
     private GameObject currentPoop;
-    public int confidenceScore;
+    public int confortScore;
     public int entertainmentScore;
     public int hygieneScore;
 
@@ -58,11 +58,12 @@ public class GameManager : MonoBehaviour
         pickedUpItems = new List<PickUpObject>();
         currentTime = maxTimeLeft;
         gameRunning = true;
-        confidenceScore = 0;
+        confortScore = 0;
         entertainmentScore = 0;
         hygieneScore = 0;
         SpawnObjects();
         UIManager.Instance.SetTime(currentTime);
+        UIManager.Instance.SetSteps(currentPlayer.currentSteps);
     }
     private void UpdateTime()
     {
@@ -90,6 +91,7 @@ public class GameManager : MonoBehaviour
         }
         else
         {
+            hygieneScore -= 100;
             UIManager.Instance.SetEnd(false, pickedUpItems);
             currentPoop = Instantiate(poopPrefab, currentPlayer.transform);
             //currentPoop.transform.position = currentPlayer.transform.position - (Vector3.down * 0.5f);
@@ -136,7 +138,7 @@ public class GameManager : MonoBehaviour
     }
     public void AddItem(PickUpObject pickUpObject)
     {
-        confidenceScore += pickUpObject.confortValue;
+        confortScore += pickUpObject.confortValue;
         entertainmentScore += pickUpObject.entertainmentValue;
         hygieneScore += pickUpObject.hygieneValue;
 
